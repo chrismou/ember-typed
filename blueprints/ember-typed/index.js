4b788d979dd1408b60636881197dd6ef6fe40b6f
Igor Preston <igorpreston@gmail.com>
2016-03-10 04:53:48 -0800

    updated blueprint

--- blueprints/ember-typed/index.js
+++ blueprints/ember-typed/index.js
@@ -1,4 +1,10 @@
 module.exports = {
+  description: 'Add bower dependencies for typed.js library',
+
+  normalizeEntityName: function() {
+
+  },
+
   afterInstall: function() {
     return this.addBowerPackagesToProject([
       { name: 'typed.js', target: '^1.1.1' }
