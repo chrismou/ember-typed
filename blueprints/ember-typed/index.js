714c769b01ea762e223511cb597692d5f817d50f
Chris Chrisostomou <dev@mou.me>
2018-03-13 23:45:39 +0000

    Update index.js

--- blueprints/ember-typed/index.js
+++ blueprints/ember-typed/index.js
@@ -7,7 +7,7 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'typed.js', target: '^1.1.1' }
+      { name: 'typed.js', target: '^2.0.6' }
     ]);
   }
 };
